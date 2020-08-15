ab933bd8bd9d900f87bfa95cf6fc85e44c0219f8
srolisp <srolisp>
2020-08-15 13:49:52 +0000

    Emacs configuration.

---
+++ .00-settings/srolisp-theme-blink.js
@@ -0,0 +1,4 @@
+// todo: rgba: a is 0.0~1.0 range
+t.prefs_.set('cursor-color', 'rgba(205, 0, 0, 0.5)'); 
+t.prefs_.set('foreground-color', 'rgba(255, 233, 122, 1)'); // #FFE97A
+t.prefs_.set('background-color', 'rgba(0, 43, 0, 1)'); // #002B00 -> 0, 43, 0
---
+++ init.el
@@ -0,0 +1,19 @@
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(org-babel-load-file
+ "~/.emacs.d/.00-settings/configuration.org")
+ 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(avy multiple-cursors lsp-ui flycheck lsp-mode elpy magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
