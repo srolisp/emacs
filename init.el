37c469fe5b9c31a3b643fdc1b0fd1ae657fd82f9
srolisp <srolisp@gmail.com>
2020-08-22 07:08:39 +0000

    Config avy and org-babel.

--- init.el
+++ init.el
@@ -10,7 +10,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(avy multiple-cursors lsp-ui flycheck lsp-mode elpy magit)))
+ '(package-selected-packages
+   '(ob-ipython ein avy multiple-cursors lsp-ui flycheck lsp-mode elpy magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
